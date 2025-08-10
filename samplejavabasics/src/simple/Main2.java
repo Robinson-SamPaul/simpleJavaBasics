71501cf1d502cfb0ec8417bbd53e3009f6af8a88
E <robinson.e@cgi.com>
2025-08-10 20:24:06 +0530

    changes done

--- samplejavabasics/src/simple/Main2.java
+++ samplejavabasics/src/simple/Main2.java
@@ -1,5 +1,7 @@
 package simple;
 
+import java.util.List;
+
 public class Main2 {
 
 	public static void main(String[] args) {
@@ -8,5 +10,22 @@ public class Main2 {
 		} else {
 			System.out.println("Dog");
 		}
+		char[] str ={'A','B','C'} ;
+        System.out.println(str);
+        System.out.println(str.toString());
+        System.out.println(String.valueOf(str)); // method overloading for charArr and otherArrs
+        System.out.println(new String(str));
+		int[] arr ={1,2,3} ;
+        System.out.println(String.valueOf(arr));
+        System.out.println(arr.toString());
+        
+        System.out.println("In my current role at CGI, I have been actively involved in implementing REST APIs and adding new fe".length());
+        
+        List.of(1, 2, 3)
+        .stream()
+        .peek(i -> System.out.println("Peek: " + i))  // Intermediate
+        .map(i -> i * 2)
+        .forEach(i -> System.out.println("ForEach: " + i)); // Terminal
+
 	}
 }
